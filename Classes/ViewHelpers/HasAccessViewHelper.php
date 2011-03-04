975c581d2f4c82568152b03b4f3774b2fde9aa66
daniellienert <daniellienert@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-04 12:57:02 +0000

    Merge branch 'master' of /var/git/mimi/rbac
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rbac/trunk@44621 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ViewHelpers/HasAccessViewHelper.php
+++ Classes/ViewHelpers/HasAccessViewHelper.php
@@ -48,6 +48,12 @@
  * </rbac:hasAccess>
  * </code>
  *
+ * The action can be a single action or a string concatenated with '||'. "action1 || action2" will be evaluated as "User has Access to action1 OR action2 on this object.
+ * <rbac:hasAccess object="rbac_object_name" action="action1 || action2">
+ * ....
+ * </rbac:hasAccess>
+ * 
+ *
  * Everything inside the "access" tag is displayed if the user has access to action on object.
  * Otherwise, everything inside the "noAccess"-tag is displayed.
  *
@@ -60,16 +66,31 @@
  *
  * @package ViewHelpers
  * @author Michael Knoll <mimi@kaktusteam.de>
+ * @author Daniel Lienert <daniel@lienert.cc>
  */
 class Tx_Rbac_ViewHelpers_HasAccessViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractConditionViewHelper {
+	
+	/**
+	 * @var Tx_Rbac_Domain_AccessControllService
+	 */
+	protected $accessControllService;
+	
 	
 	/**
 	 * Initialize arguments
 	 */
     public function initializeArguments() {
         $this->registerArgument('feUser', 'Tx_Extbase_Domain_Model_FrontendUser', 'Frontend-User to check access rights for', FALSE);
-        $this->registerArgument('object', 'string', 'Object to check if user has access rights for', TRUE);
-        $this->registerArgument('action', 'string', 'Action to check if user has access rights for', TRUE);
+        $this->registerArgument('object', 'string', 'Object to check if user has access rights for,', TRUE);
+        $this->registerArgument('action', 'string', 'Action(s) to check if user has access rights for.', TRUE);
+        
+        if ($this->arguments['feUser'] === null) {
+			$feUser = $this->getLoggedInFrontendUser();
+		} else {
+			$feUser = $this->arguments['feUser'];
+		}
+		
+		$this->accessControllService = Tx_Rbac_Domain_AccessControllServiceFactory::getInstance($feUser);
     }
 
     
@@ -79,20 +100,33 @@ class Tx_Rbac_ViewHelpers_HasAccessViewHelper extends Tx_Fluid_Core_ViewHelper_A
 	 * 
 	 * @return string Rendered hasAccess ViewHelper
 	 */
-	public function render() {
-		if ($this->arguments['feUser'] === null) {
-			$feUser = $this->getLoggedInFrontendUser();
-		} else {
-			$feUser = $this->arguments['feUser'];
-		}
-    	$accessControllService = Tx_Rbac_Domain_AccessControllServiceFactory::getInstance($feUser);
-		if ($accessControllService->loggedInUserHasAccess($this->arguments['object'], $this->arguments['action'])) {
+	public function render() {		
+    	if ($this->checkAccess($this->arguments['object'], $this->arguments['action'])) {
 			return $this->renderThenChild();
 		} else {
 			return $this->renderElseChild();
 		}
 	}
 	
+	
+	/**
+	 * Check if the user has the rigth to do an action on an object
+	 * 
+	 * @param unknown_type $object
+	 * @param unknown_type $actionString
+	 */
+	protected function checkAccess($object, $actionString) {
+		$access = false;
+		$actions = explode('||', $actionString);
+		
+		foreach($actions as $action) {
+			if($this->accessControllService->loggedInUserHasAccess($object, trim($action))) {
+				$access = true;
+			}
+		}
+		
+		return $access;
+	}
 	
 	
 	/**
